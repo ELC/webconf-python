ead52741b68c949db6e4df96ba7c84ae26025db8
Ezequiel Leonardo Castaño <elcg@gmx.com>
2021-08-23 01:40:30 -0300

    Reduce unnecessary configuration

--- 03_user_interface/frontend/vue.config.js
+++ 03_user_interface/frontend/vue.config.js
@@ -1,16 +1,5 @@
 const path = require("path");
 
 module.exports = {
-  devServer: {
-    proxy: {
-      '^/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-        logLevel: 'debug',
-        pathRewrite: { '^/api': '/' },
-      },
-    },
-  },
-  outputDir: path.resolve(__dirname, "build/"),
-  assetsDir: "."
+  outputDir: path.resolve(__dirname, "build/")
 }
